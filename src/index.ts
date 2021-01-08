b1c3b20a5a16ab76da01e0a22bae2cf4c957f5ff
Dusan Minic <dusan.minic@tradecore.com>
2021-01-08 18:57:45 +0100

    Added express framework

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import "reflect-metadata";
+import express, { Request, Response } from 'express';
+
+const PORT = 3000;
+
+const application = express();
+
+application.get('/', (request: Request, response: Response) => {
+    response.send('ryd assignment');
+});
+
+application.listen(PORT, () => {
+    console.log(`Server started listening at ${PORT}.`);
+});
